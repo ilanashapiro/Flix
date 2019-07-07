3fbbe9fb0559d286fc5102d070df788ef4d8f801
Ilana Shapiro <ilanashapiro@fb.com>
2019-07-07 00:08:21 -0700

    forgot too add a method to fetch Popular Movies from the Movie Database API in MovieApiManager.m

--- Flix/API/MovieApiManager.m
+++ Flix/API/MovieApiManager.m
@@ -12,6 +12,7 @@
 @interface MovieApiManager()
 
 @property (nonatomic, strong) NSURLSession *session;
+@property (nonatomic, strong) NSString *APIKey;
 
 @end
 
@@ -20,13 +21,15 @@
 - (id)init {
     self = [super init];
     
+    self.APIKey = @"a07e22bc18f5cb106bfe4cc1f83ad8ed";
     self.session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     
     return self;
 }
 
-- (void)fetchNowPlaying:(void(^)(NSMutableArray *movies, NSError *error))completion {
-    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+- (void)fetchMoviesWithUrl:(NSURL *)url completion:(void(^)(NSMutableArray *movies, NSError *error))completion {
+    //helper function to avoid repeating code in fetchNowPlaying and fetchPopular
+    
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSessionDataTask *task = [self.session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         if (error != nil) {
@@ -50,6 +53,20 @@
         }
     }];
     [task resume];
+}
+
+- (void)fetchNowPlaying:(void(^)(NSMutableArray *movies, NSError *error))completion {
+    //fetch now playing movies from The Movie Database API
+    
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", @"https://api.themoviedb.org/3/movie/now_playing?api_key=", self.APIKey]];
+    [self fetchMoviesWithUrl:url completion:completion];
+}
+
+- (void)fetchPopular:(void(^)(NSMutableArray *movies, NSError *error))completion {
+    //fetch popular movies from The Movie Database API
+    
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", @"https://api.themoviedb.org/3/movie/popular?api_key=", self.APIKey]];
+    [self fetchMoviesWithUrl:url completion:completion];
 }
 
 @end
